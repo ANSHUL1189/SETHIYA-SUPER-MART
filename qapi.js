eae241a14ee400957945421b1d942ba6b9cabac6
ANSHUL1189 <81228353+ANSHUL1189@users.noreply.github.com>
2021-03-23 15:24:52 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6059b76ecb40ac001fc61b64",
+				"6059b76ecb40ac001fc61b66"
+			]
+		},
+		"6059b76ecb40ac001fc61b64": {
+			"id": "6059b76ecb40ac001fc61b64",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6059b76ecb40ac001fc61b66": {
+			"id": "6059b76ecb40ac001fc61b66",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
